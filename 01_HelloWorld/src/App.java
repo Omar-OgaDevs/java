539f0ad82ba1971fae9426a295175786f14d0a47
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-08 18:46:43 -0500

    primitive values

--- 01_HelloWorld/src/App.java
+++ 01_HelloWorld/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("¡Hoooolaaaa Muuuundoooooo!");
+
+        int myFirstNumber = (10+5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;        
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
