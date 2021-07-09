4b5b78d178769ed26d47645aa4cd4146ac99dfd7
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-09 14:31:59 -0500

    Char and Boolean

---
+++ 06_CharAndBoolean/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        char myChar = 'E';
+        char myUnicodeChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
