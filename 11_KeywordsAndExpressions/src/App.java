07280244aae16d128b32385173733a591cea130c
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-12 12:04:15 -0500

    keywords and expressions

---
+++ 11_KeywordsAndExpressions/src/App.java
@@ -0,0 +1,19 @@
+//Java has 53 keywords
+//There are 4 important concepts in Java
+//1. Expressions
+//2. Statements
+//3. Code Blocks
+//4. Methods
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //a mile is equal to 1.609344 kilometers
+        double kilometers = (100*1.609344);
+
+        int highScore = 50;
+
+        if(highScore == 50){
+
+        }
+    }
+}
