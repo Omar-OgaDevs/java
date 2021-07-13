faf9887996fa3f65432e4859e2046937c978136d
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-13 15:31:13 -0500

    fix

---
+++ 16_MegabytesConverter/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        printMegaBytesAndKiloBytes(2048);
+        printMegaBytesAndKiloBytes(12321312);
+        printMegaBytesAndKiloBytes(-213);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int remainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB");
+        }
+    }
+}
+    
+   
+
