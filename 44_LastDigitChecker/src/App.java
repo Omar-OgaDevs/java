86f9b0c4f6430cfc2beb25c9e55bc4a4b21cd992
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-15 12:23:05 -0500

    last digit checker

---
+++ 44_LastDigitChecker/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(231, 372, 423));
+        System.out.println(hasSameLastDigit(91, 991, 532));
+    }
+
+    public static boolean hasSameLastDigit (int firstNumber, int secondNumber, int thirdNumber) {
+        if ((firstNumber >= 10 && firstNumber <= 1000) && (secondNumber >= 10 && secondNumber <= 1000) && (thirdNumber >= 10 && thirdNumber <= 1000)) {
+            System.out.println("Do any of given numbers (" + firstNumber + ", " + secondNumber + ", " + thirdNumber + ") share a last digit?");
+            return ((firstNumber % 10 == secondNumber % 10) || (firstNumber % 10 == thirdNumber % 10) || (secondNumber % 10 == thirdNumber % 10));
+        }
+        System.out.println("Invalid input");
+        return false;
+    }
+}
