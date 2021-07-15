296b13fa529779e35199ea4bbf82d89fe7286995
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-15 14:27:04 -0500

    problems and solutions

--- 50_ReadingUserInput/src/App.java
+++ 50_ReadingUserInput/src/App.java
@@ -2,20 +2,30 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.println("Enter yout name: ");
-        String name = scanner.nextLine();
+        boolean hasNextInt = scanner.hasNextInt();
 
-        int age = 2021 - yearOfBirth;
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            System.out.println("Enter yout name: ");
+            String name = scanner.nextLine();
+            int age = 2021 - yearOfBirth;
 
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
         scanner.close();
     }
 }
