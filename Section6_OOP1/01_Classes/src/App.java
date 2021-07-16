73ed6628a756f5fd855e99c0c96e5e925bb39bf0
omar.ogadevs <omar.ogadevs@outlook.com>
2021-07-16 18:06:15 -0500

    classes

---
+++ Section6_OOP1/01_Classes/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Car lamborghini = new Car();
+        Car bmw = new Car();
+        lamborghini.setModel("Murcielago");
+        System.out.println("Model is " + lamborghini.getModel());
+        
+    }    
+}
