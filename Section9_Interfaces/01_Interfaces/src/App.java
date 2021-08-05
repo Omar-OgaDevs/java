ea41ef43afc4b21105745ff922b381895ccba299
omar.ogadevs <omar.ogadevs@outlook.com>
2021-08-05 11:12:39 -0500

    deskphone mobilephone

--- Section9_Interfaces/01_Interfaces/src/App.java
+++ Section9_Interfaces/01_Interfaces/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        ITelephone omarsPhone;
+        omarsPhone = new DeskPhone(29767457);
+        omarsPhone.powerOn();
+        omarsPhone.callPhone(29767457);
+        omarsPhone.answer();
+
+        omarsPhone = new MobilePhone(29767457);
+        omarsPhone.powerOn();
+        omarsPhone.callPhone(29767457);
+        omarsPhone.answer();
     }
 }
