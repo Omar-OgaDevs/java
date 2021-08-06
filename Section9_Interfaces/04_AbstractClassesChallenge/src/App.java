b2aa211b078064e81a786f5a517234e59e3afe5a
omar.ogadevs <omar.ogadevs@outlook.com>
2021-08-06 12:22:18 -0500

    binary search tree

--- Section9_Interfaces/04_AbstractClassesChallenge/src/App.java
+++ Section9_Interfaces/04_AbstractClassesChallenge/src/App.java
@@ -1,39 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        MyLinkedList list = new MyLinkedList(null);
-        list.traverse(list.getRoot());
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
 
         String stringData = "7 5 9 3 4 2 7 4 8 0 1 6 3";
 
         String[] data = stringData.split(" ");
         for (String s : data) {
             // Create new item with value set to the string s
-            list.addItem(new Node(s));
-        }
+            tree.addItem(new Node(s));
+        }                
 
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("3"));
-        list.traverse(list.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("3"));
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("5"));
-        list.traverse(list.getRoot());
+        tree.removeItem(new Node("5"));
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("0"));
-        list.removeItem(new Node("4"));
-        list.removeItem(new Node("2"));
-        list.traverse(list.getRoot());
+        tree.removeItem(new Node("0"));
+        tree.removeItem(new Node("4"));
+        tree.removeItem(new Node("2"));
+        tree.traverse(tree.getRoot());
 
-        list.removeItem(new Node("9"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("8"));
-        list.traverse(list.getRoot());
-        list.removeItem(new Node("6"));
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
-        list.removeItem(list.getRoot());
-        list.traverse(list.getRoot());
+        tree.removeItem(new Node("9"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("8"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("6"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
 
     }
 }
