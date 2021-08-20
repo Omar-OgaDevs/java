1f83a9c9cb2538a4e6511f00fe8099aa7391e6c5
omar.ogadevs <omar.ogadevs@outlook.com>
2021-08-20 10:54:48 -0500

    Scope

---
+++ Section10_JavaGenerics/03_Scope/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String varFour = "this is private to the main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        scopeInstance.useInner();
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        System.out.println("varThree is not accessible here " + innerClass.varThree);
+
+        /* System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+        System.out.println(varFour);
+
+        scopeInstance.timesTwo();
+        System.out.println("***************************");
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+    } */
+}
+
+}
